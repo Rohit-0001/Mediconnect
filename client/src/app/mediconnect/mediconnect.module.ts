a90c09f24585ba9988e926b693f018032f834e87
Rohit-0001 <rohitsajwan003@gmail.com>
2026-05-04 15:07:57 +0000

    Day 22

--- client/src/app/mediconnect/mediconnect.module.ts
+++ client/src/app/mediconnect/mediconnect.module.ts
@@ -1,28 +1,29 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-
-import { MediConnectRoutingModule } from "./mediconnect-routing.module";
-import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { PatientCreateComponent } from "./components/patientcreate/patientcreate.component";
-import { DoctorCreateComponent } from "./components/doctorcreate/doctorcreate.component";
 import { ClinicCreateComponent } from "./components/cliniccreate/cliniccreate.component";
+import { DoctorCreateComponent } from "./components/doctorcreate/doctorcreate.component";
+import { AppointmentCreateComponent } from "./components/appointment/appointment.component";
+import { MediconnectRoutingModule } from "./mediconnect-routing.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
     PatientCreateComponent,
     DoctorCreateComponent,
-    ClinicCreateComponent
+    ClinicCreateComponent,
+    AppointmentCreateComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
     HttpClientModule,
+    MediconnectRoutingModule,
+    RouterModule
   ],
   exports: [
-    PatientCreateComponent,
-    DoctorCreateComponent,
-    ClinicCreateComponent
   ]
 })
 export class MediconnectModule {}
